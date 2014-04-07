02c34565fd9b0a23746fb564e8b99990b1702442
psionin <psionin@yahoo.com>
2014-04-07 19:35:21 +0000

    network protocol version / min version update

--- src/version.h
+++ src/version.h
@@ -31,10 +31,10 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60003;
+static const int PROTOCOL_VERSION = 60004;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 209;
+static const int MIN_PROTO_VERSION = 60003;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
