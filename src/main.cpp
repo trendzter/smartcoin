7bb3f9cf7e52a2f61afc72ee16c38da5e2d65248
psionin <psionin@yahoo.com>
2015-02-16 19:08:30 +0300

    switching to DGW3 for difficulty adjustment at block 385000

--- src/main.cpp
+++ src/main.cpp
@@ -1088,6 +1088,7 @@ int64 nTargetSpacing = 40; // 40 second blocks
 static const int64 forkBlock1 = 35000; // February 2014 fork
 static const int64 forkBlock2 = 200000; // April 2014 fork
 static const int64 forkBlock3 = 300000; // July 2014 fork
+static const int64 forkBlock4 = 385000; // February 2015 fork
 int64 static GetBlockValue(int nHeight, int64 nFees)
 {
     int64 nSubsidy = 64 * COIN;
@@ -1374,23 +1375,84 @@ unsigned int static DigiShield(const CBlockIndex* pindexLast, const CBlockHeader
     return bnNew.GetCompact();
 }
 
+unsigned int static DarkGravityWave(const CBlockIndex* pindexLast, const CBlockHeader *pblock) {
+	/* current difficulty formula, darkcoin - DarkGravity v3, written by Evan Duffield - evan@darkcoin.io */
+	const CBlockIndex *BlockLastSolved = pindexLast;
+	const CBlockIndex *BlockReading = pindexLast;
+	const CBlockHeader *BlockCreating = pblock;
+	BlockCreating = BlockCreating;
+	int64_t nActualTimespan = 0;
+	int64_t LastBlockTime = 0;
+	int64_t PastBlocksMin = 24;
+	int64_t PastBlocksMax = 24;
+	int64_t CountBlocks = 0;
+	CBigNum PastDifficultyAverage;
+	CBigNum PastDifficultyAveragePrev;
+
+	if (BlockLastSolved == NULL || BlockLastSolved->nHeight == 0 || BlockLastSolved->nHeight < PastBlocksMin) {
+		return bnProofOfWorkLimit.GetCompact();
+	}
+
+	for (unsigned int i = 1; BlockReading && BlockReading->nHeight > 0; i++) {
+		if (PastBlocksMax > 0 && i > PastBlocksMax) { break; }
+		CountBlocks++;
+
+		if(CountBlocks <= PastBlocksMin) {
+			if (CountBlocks == 1) { PastDifficultyAverage.SetCompact(BlockReading->nBits); }
+			else { PastDifficultyAverage = ((PastDifficultyAveragePrev * CountBlocks)+(CBigNum().SetCompact(BlockReading->nBits))) / (CountBlocks+1); }
+			PastDifficultyAveragePrev = PastDifficultyAverage;
+		}
+
+		if(LastBlockTime > 0){
+			int64_t Diff = (LastBlockTime - BlockReading->GetBlockTime());
+			nActualTimespan += Diff;
+		}
+		LastBlockTime = BlockReading->GetBlockTime();
+
+		if (BlockReading->pprev == NULL) { assert(BlockReading); break; }
+		BlockReading = BlockReading->pprev;
+	}
+
+	CBigNum bnNew(PastDifficultyAverage);
+
+	int64_t _nTargetTimespan = CountBlocks*nTargetSpacing;
+
+	if (nActualTimespan < _nTargetTimespan/3)
+		nActualTimespan = _nTargetTimespan/3;
+	if (nActualTimespan > _nTargetTimespan*3)
+		nActualTimespan = _nTargetTimespan*3;
+
+	// Retarget
+	bnNew *= nActualTimespan;
+	bnNew /= _nTargetTimespan;
+
+	if (bnNew > bnProofOfWorkLimit) {
+		bnNew = bnProofOfWorkLimit;
+	}
+
+	return bnNew.GetCompact();
+}
+
 unsigned int static GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock)
 {
     int DiffMode = 1;
 
     if (fTestNet) {
-        if (pindexLast->nHeight+1 >= 50) { DiffMode = 2; }
-		if (pindexLast->nHeight+1 >= 100) { DiffMode = 3; }
+        if (pindexLast->nHeight+1 >= 10) { DiffMode = 2; }
+		if (pindexLast->nHeight+1 >= 20) { DiffMode = 3; }
+		if (pindexLast->nHeight+1 >= 30) { DiffMode = 4; }
     }
     else {
         if (pindexLast->nHeight+1 >= forkBlock1 && pindexLast->nHeight+1 < forkBlock2) { DiffMode = 2; }
-        else if (pindexLast->nHeight+1 > forkBlock2) { DiffMode = 3; }
+		else if (pindexLast->nHeight+1 > forkBlock2 && pindexLast->nHeight+1 < forkBlock4) { DiffMode = 3; }
+		else if (pindexLast->nHeight+1 >= forkBlock4) { DiffMode = 4; }
     }
 
     switch (DiffMode) {
         case 1: return GetNextWorkRequired_V1(pindexLast, pblock);
         case 2: return KimotoGravityWell(pindexLast, pblock);
         case 3: return DigiShield(pindexLast, pblock);
+		case 4: return DarkGravityWave(pindexLast, pblock);
         default: return GetNextWorkRequired_V1(pindexLast, pblock);
     }
 }
